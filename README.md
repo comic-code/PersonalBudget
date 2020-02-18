8994b4986e9b5f00ee91ae098fc0008d9f5a919a
comicodarko <bitotajima@hotmail.com>
2020-02-18 02:48:04 -0300

    Feito -  Gravação no LocalStorage com id autoincrementável

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PersonalBudget
-Uma aplicação WEB de orçamento pessoal usando JavaScript e Local Storage. O mesmo permite o cadastro, a consulta e a exclusão de despesas :)
+Uma aplicação WEB de orçamento pessoal usando JavaScript Orientado a Objeto e Local Storage. O mesmo permite o cadastro, a consulta e a exclusão de despesas :)
 
---
+++ app.js
@@ -0,0 +1,60 @@
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.valor = valor;
+  }
+}
+
+class Bd {
+
+  constructor() {
+    let id = localStorage.getItem('id');
+  
+    // Lógica do id
+    if(id === null) {
+      localStorage.setItem('id', 0);
+    }
+  }
+
+  getProximoId() {
+    let proximoId = localStorage.getItem('id')
+    return parseInt(proximoId) + 1;
+  }
+
+  gravar(d) {
+    // É necessário transformar o objeto literal em JSON
+    let id = this.getProximoId();
+    localStorage.setItem(id, JSON.stringify(d));
+
+    localStorage.setItem('id', id);
+ }
+}
+
+let bd = new Bd()
+
+function cadastrarDespesa() {
+
+  let ano = document.getElementById('ano');
+  let mes = document.getElementById('mes');
+  let dia = document.getElementById('dia');
+  let tipo = document.getElementById('tipo');
+  let descricao = document.getElementById('descricao');
+  let valor = document.getElementById('valor');
+
+  let despesa = new Despesa(
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
+    valor.value,
+  )
+
+  bd.gravar(despesa);
+
+}
+
